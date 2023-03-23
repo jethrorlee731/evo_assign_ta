994a21b66d6130319b9d7b00a19fa9aa2397edd5
Michelle Wang <michellewang0402@gmail.com>
2023-03-23 16:09:06 -0400

    trying to work on unit test

--- tests/test_assign_ta.py
+++ tests/test_assign_ta.py
@@ -3,54 +3,68 @@ Unit tests for assign_ta.py
 """
 
 import pytest
+from evo import Evo
 import assign_ta
 
-# tagging it as a fixture - it knows when it sees stack
-# it is the function it needs to call
-@pytest.fixture
-# function that creates the object
-def evo():
-    return Evo()
 
-# should have a test for each method from stack.py
+def test_overallocation():
+    pass
 
-# test the __init__
-def test_constructor():
-    # verify that an instance is created
-    s = Stack()
-    assert isinstance(s, Stack), "Did not construct a stack"
+def test_conflicts():
+    pass
 
-    # check to see that stack is empty
-    assert s.size()==0, "Stack is not empty"
+def test_undersupport():
+    pass
 
-    # this works after doing __len__
-    assert len(s) == 0, "Stack is not empty"
+def test_unwilling():
+    pass
 
-# parameter is the stack function
-def test_push(stack):
-    # s = Stack()
-    stack.push(3)
-    stack.push(4)
-    assert len(stack) == 2
-    assert stack.top() == 4
-
-def test_pop(stack):
-    stack.push('a')
-    stack.push('b')
-    assert stack.pop() == 'b', 'wrong value popped'
-    assert stack.pop() == 'a', 'wrong value popped'
-    assert stack.pop() is None, 'wrong value popped'
-    assert stack.top() is None, 'Stack should be empty but it is not'
+def test_unpreferred():
+    pass
 
 
-def test_top(stack):
-    stack.push(1)
-    stack.push(2)
-    stack.push(3)
-    assert stack.top() == 3, 'wrong value on the top'
-    stack.pop()
-    stack.pop()
-    assert stack.top() == 1, 'wrong value on the top'
-    stack.pop()
-    assert stack.top() is None, 'Stack should be empty '
-
+# @pytest.fixture
+# # function that creates the object
+# def evo():
+#     return Evo()
+#
+# # test the __init__
+# def test_constructor():
+#     # verify that an instance is created
+#     e = Evo()
+#     assert isinstance(e, Evo), "Did not construct a stack"
+#
+#     # check to see that stack is empty
+#     assert evo.size() == 0, "Stack is not empty"
+#
+#     # this works after doing __len__
+#     assert len(evo) == 0, "Stack is not empty"
+#
+# # parameter is the stack function
+# def test_push(evo):
+#     # s = Stack()
+#     evo.push(3)
+#     evo.push(4)
+#     assert len(evo) == 2
+#     assert evo.top() == 4
+#
+# def test_pop(evo):
+#     evo.push('a')
+#     evo.push('b')
+#     assert evo.pop() == 'b', 'wrong value popped'
+#     assert evo.pop() == 'a', 'wrong value popped'
+#     assert evo.pop() is None, 'wrong value popped'
+#     assert evo.top() is None, 'Stack should be empty but it is not'
+#
+#
+# def test_top(evo):
+#     evo.push(1)
+#     evo.push(2)
+#     evo.push(3)
+#     assert evo.top() == 3, 'wrong value on the top'
+#     evo.pop()
+#     evo.pop()
+#     assert evo.top() == 1, 'wrong value on the top'
+#     evo.pop()
+#     assert evo.top() is None, 'Stack should be empty '
+#
