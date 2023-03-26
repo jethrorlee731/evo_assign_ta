3dbe4d6c1221c79cfb7b81966a947fbda3735a4e
Michelle Wang <michellewang0402@gmail.com>
2023-03-26 16:40:36 -0400

    fixed unit tests

--- tests/test_assign_ta.py
+++ tests/test_assign_ta.py
@@ -24,42 +24,42 @@ def test_overallocation(evo):
     """
     Check that the overallocation function is working correctly
     """
-    assert evo.overallocation(test1, tas[:, 2]) == 37
-    assert evo.overallocation(test2, tas[:, 2]) == 41
-    assert evo.overallocation(test3, tas[:, 2]) == 23
+    assert evo.overallocation(test1) == 37
+    assert evo.overallocation(test2) == 41
+    assert evo.overallocation(test3) == 23
 
 
 def test_conflicts(evo):
     """
     Check that conflicts function is working correctly
     """
-    assert evo.conflicts(test1, sections[:, 2]) == 8
-    assert evo.conflicts(test2, sections[:, 2]) == 5
-    assert evo.conflicts(test3, sections[:, 2]) == 2
+    assert evo.conflicts(test1) == 8
+    assert evo.conflicts(test2) == 5
+    assert evo.conflicts(test3) == 2
 
 
 def test_undersupport(evo):
     """
     Check that undersupport function is working correctly
     """
-    assert evo.undersupport(test1, sections[:, 6]) == 1
-    assert evo.undersupport(test2, sections[:, 6]) == 0
-    assert evo.undersupport(test3, sections[:, 6]) == 7
+    assert evo.undersupport(test1) == 1
+    assert evo.undersupport(test2) == 0
+    assert evo.undersupport(test3) == 7
 
 
 def test_unwilling(evo):
     """
     Check that unwilling function is working correctly
     """
-    assert evo.unwilling(test1, sections[1:, 3:]) == 53
-    assert evo.unwilling(test2, sections[1:, 3:]) == 58
-    assert evo.unwilling(test3, sections[1:, 3:]) == 43
+    assert evo.unwilling(test1) == 53
+    assert evo.unwilling(test2) == 58
+    assert evo.unwilling(test3) == 43
 
 
 def test_unpreferred(evo):
     """
     Check that unpreferred function is working correctly
     """
-    assert evo.unpreferred(test1, sections[1:, 3:]) == 15
-    assert evo.unpreferred(test2, sections[1:, 3:]) == 19
-    assert evo.unpreferred(test3, sections[1:, 3:]) == 10
+    assert evo.unpreferred(test1) == 15
+    assert evo.unpreferred(test2) == 19
+    assert evo.unpreferred(test3) == 10
